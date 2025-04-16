5702d0550777b2b78ab9c5849f04883ec6d02518
jcaparr <juancaparr0s@hotmail.com>
2025-04-16 10:52:32 -0300

    chore(Service implementations package, Controller package):

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/controller/PostController.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/controller/PostController.java
@@ -1,11 +1,11 @@
 package com.mercadolibre.be_java_hisp_w31_g07.controller;
 
-import com.mercadolibre.be_java_hisp_w31_g07.service.IUserService;
+import com.mercadolibre.be_java_hisp_w31_g07.service.IPostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class UserController {
-    private final IUserService userService;
+public class PostController {
+    private final IPostService postService;
 }
--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
@@ -1,8 +1,12 @@
 package com.mercadolibre.be_java_hisp_w31_g07.service.implementations;
 
+import com.mercadolibre.be_java_hisp_w31_g07.repository.ISellerRepository;
 import com.mercadolibre.be_java_hisp_w31_g07.service.ISellerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SellerService implements ISellerService {
+    private final ISellerRepository sellerRepository;
 }
