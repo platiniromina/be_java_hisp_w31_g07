794c0d8462a6d1ccb3f26eb61e3f9378fec59acb
platini.romina <platini.romina@gmail.com>
2025-04-16 15:21:10 -0300

    fix(GlobalExceptionHandler): add controller advice annotation

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/exception/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package com.mercadolibre.be_java_hisp_w31_g07.exception;
 import com.mercadolibre.be_java_hisp_w31_g07.dto.GlobalExceptioHandlerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(BadRequest.class)
     public ResponseEntity<?> notFound(BadRequest e){
