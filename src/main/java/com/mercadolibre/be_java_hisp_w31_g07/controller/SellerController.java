72a0d436b517032044eee6cde48993b28bc3bee7
jcaparr <juancaparr0s@hotmail.com>
2025-04-17 15:03:12 -0300

    feat(SellerController): add IllegalArgumentException handler for a userId's BadRequest.

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/controller/SellerController.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/controller/SellerController.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.be_java_hisp_w31_g07.controller;
 
+import com.mercadolibre.be_java_hisp_w31_g07.exception.BadRequest;
 import com.mercadolibre.be_java_hisp_w31_g07.service.ISellerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -16,8 +17,14 @@ public class SellerController {
     private final ISellerService sellerService;
 
     @GetMapping("/users/{userId}/followers/list")
-        public ResponseEntity<?> getFollowers(@PathVariable UUID userId){
-            return new ResponseEntity<>(sellerService.findFollowers(userId), HttpStatus.OK);
+        public ResponseEntity<?> getFollowers(@PathVariable String userId){
+            UUID userUuid;
+            try{
+                userUuid = UUID.fromString(userId);
+            }catch (IllegalArgumentException e){
+                throw new BadRequest("Error en el formato del id");
+            }
+            return new ResponseEntity<>(sellerService.findFollowers(userUuid), HttpStatus.OK);
         }
 
 }
