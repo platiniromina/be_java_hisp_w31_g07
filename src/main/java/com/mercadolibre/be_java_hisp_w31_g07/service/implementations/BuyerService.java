3b4903be6d5ca8a17c7c95631d8bfc8a07a897e5
jcaparr <juancaparr0s@hotmail.com>
2025-04-18 19:58:46 -0300

    chore(BuyerService): refactor how to throw a not found exception.

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/BuyerService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/BuyerService.java
@@ -2,33 +2,27 @@ package com.mercadolibre.be_java_hisp_w31_g07.service.implementations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mercadolibre.be_java_hisp_w31_g07.dto.request.BuyerDto;
-import com.mercadolibre.be_java_hisp_w31_g07.dto.request.SellerDto;
-import com.mercadolibre.be_java_hisp_w31_g07.dto.response.BuyerReponseDto;
 import com.mercadolibre.be_java_hisp_w31_g07.dto.response.SellerResponseDto;
 import com.mercadolibre.be_java_hisp_w31_g07.exception.NotFoundException;
 import com.mercadolibre.be_java_hisp_w31_g07.model.Buyer;
 import com.mercadolibre.be_java_hisp_w31_g07.repository.IBuyerRepository;
+import com.mercadolibre.be_java_hisp_w31_g07.repository.IUserRepository;
 import com.mercadolibre.be_java_hisp_w31_g07.service.IBuyerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class BuyerService implements IBuyerService {
     private final IBuyerRepository buyerRepository;
+    private final IUserRepository userRepository;
 
     @Override
     public BuyerDto findFollowed(UUID userId){
         ObjectMapper mapper = new ObjectMapper();
-        Optional<Buyer> buyerOptional = buyerRepository.findFollowed(userId);
+        Buyer buyer = buyerRepository.findFollowed(userId).orElseThrow(()-> new NotFoundException("No se encontro al usuario"));
 
-        if(buyerOptional.isEmpty()){
-            throw new NotFoundException("No se encontro al usuario");
-        }
-        Buyer buyer = buyerOptional.get();
         return new BuyerDto(buyer.getId(),
                 buyer.getFollowed().stream()
                         .map(x->mapper.convertValue(x, SellerResponseDto.class))
