139d2919e302cbe5994f28aa65e3def23d39fa54
jcaparr <juancaparr0s@hotmail.com>
2025-04-18 19:44:38 -0300

    chore(SellerService): refactor how to throw a not found exception.

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
@@ -3,15 +3,12 @@ package com.mercadolibre.be_java_hisp_w31_g07.service.implementations;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mercadolibre.be_java_hisp_w31_g07.dto.request.SellerDto;
 import com.mercadolibre.be_java_hisp_w31_g07.dto.response.BuyerReponseDto;
-import com.mercadolibre.be_java_hisp_w31_g07.dto.response.SellerResponseDto;
 import com.mercadolibre.be_java_hisp_w31_g07.exception.NotFoundException;
 import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
 import com.mercadolibre.be_java_hisp_w31_g07.repository.ISellerRepository;
 import com.mercadolibre.be_java_hisp_w31_g07.service.ISellerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -22,12 +19,9 @@ public class SellerService implements ISellerService {
     @Override
     public SellerDto findFollowers(UUID userId){
         ObjectMapper mapper = new ObjectMapper();
-        Optional<Seller> sellerOptional = sellerRepository.findFollowers(userId);
 
-        if(sellerOptional.isEmpty()){
-            throw new NotFoundException("No se encontro el usuario");
-        }
-        Seller seller = sellerOptional.get();
+        Seller seller = sellerRepository.findFollowers(userId)
+                .orElseThrow(()-> new NotFoundException("No se encontro el usuario"));
 
         return new SellerDto(seller.getId(), seller.getBillingAddress(),
                 seller.getCuil(),seller.getFollowers().stream()
