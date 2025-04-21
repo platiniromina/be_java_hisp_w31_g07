c991b3a6540e5eb24c2c78ce201157e4cd00a93c
thomasbarenghi <thomasbarenghi@gmail.com>
2025-04-21 16:13:20 -0300

    fix: remove logs

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
@@ -60,8 +60,6 @@ public class SellerService implements ISellerService {
     private void validateNotAlreadyFollowing(Buyer buyer, Seller seller) {
         boolean isFollowing = sellerRepository.sellerIsBeingFollowedByBuyer(buyer, seller.getId()); 
         boolean isFollowedBy = buyerService.buyerIsFollowingSeller(seller, buyer.getId());
-        System.out.println("isFollowing: " + isFollowing);
-        System.out.println("isFollowedBy: " + isFollowedBy);
         if (isFollowing && isFollowedBy)
             throw new BadRequest("Buyer " + buyer.getId() + " already follows seller " + seller.getId());
     }
