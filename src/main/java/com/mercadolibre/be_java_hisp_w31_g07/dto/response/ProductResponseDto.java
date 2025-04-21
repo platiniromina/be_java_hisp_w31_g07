7772997c5ddd1bb2dd01d5d188b5bea92235f3a8
platini.romina <platini.romina@gmail.com>
2025-04-21 11:08:40 -0300

    chore(create post): create ProductResponseDto class

---
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/dto/response/ProductResponseDto.java
@@ -0,0 +1,21 @@
+package com.mercadolibre.be_java_hisp_w31_g07.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductResponseDto {
+    private UUID id;
+    private String productName;
+    private String type;
+    private String brand;
+    private String color;
+    private String note;
+}
