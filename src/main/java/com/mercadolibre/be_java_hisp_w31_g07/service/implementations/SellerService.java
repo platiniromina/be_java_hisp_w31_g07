b653ede0025c95df18a79bf497ad941c1688b495
platini.romina <platini.romina@gmail.com>
2025-04-22 10:14:20 -0300

    chore(IPostService, SellerService): delete testing annotation and update method docString

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
@@ -31,10 +31,6 @@ public class SellerService implements ISellerService {
         buyerService.addSellerToFollowedList(seller, buyerId);
     }
 
-    // FOR TESTING PURPOSES ONLY
-    // This endpoint is not part of the original requirements
-    // and is only used to verify the functionality of the followSeller method.
-    // It should be removed in the final version of the code.
     @Override
     public Seller findSellerById(UUID sellerId) {
         return getSellerById(sellerId);
