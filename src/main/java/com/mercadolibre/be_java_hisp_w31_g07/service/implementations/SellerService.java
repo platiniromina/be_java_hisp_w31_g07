4c05be6c2b29719ab4f909024c7a89391fc58aa8
Delfina444 <delfina.tognolini@mercadolibre.com>
2025-04-22 17:46:52 -0300

    US09 y US08: fixed features

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/service/implementations/SellerService.java
@@ -139,14 +139,16 @@ public class SellerService implements ISellerService {
 
     private SellerDto mapToSellerDto(Seller seller) {
         Map<UUID, String> userNames = new HashMap<>();
+        userNames.computeIfAbsent(seller.getId(), id -> userService.findById(id).getUserName());
+        String sellerUsername = userNames.get(seller.getId());
+
         List<BuyerResponseDto> followers = seller.getFollowers().stream()
                 .map(buyer -> {
                     userNames.computeIfAbsent(buyer.getId(), id -> userService.findById(id).getUserName());
                     String userName = userNames.get(buyer.getId());
-                    return new BuyerResponseDto(buyer.getId(), userName, new ArrayList<>());})
+                    return new BuyerResponseDto(buyer.getId(), userName, new ArrayList<>());
+                })
                 .toList();
-
-        String sellerUsername = userNames.get(seller.getId());
 
         return new SellerDto(
                 seller.getId(),
@@ -154,6 +156,7 @@ public class SellerService implements ISellerService {
                 followers,
                 seller.getFollowerCount());
     }
+
 
     private Comparator<Buyer> getComparatorForOrder(String order) {
         Comparator<Buyer> comparator = Comparator.comparing(
