4e2b03aeed3cc987ab6dbc59221241b23e68fe78
thomasbarenghi <thomasbarenghi@gmail.com>
2025-04-29 23:05:02 -0300

    refactor: improve factories

--- src/test/java/com/mercadolibre/be_java_hisp_w31_g07/util/SellerFactory.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g07/util/SellerFactory.java
@@ -1,11 +1,8 @@
 package com.mercadolibre.be_java_hisp_w31_g07.util;
 
-import com.mercadolibre.be_java_hisp_w31_g07.model.Buyer;
 import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 public class SellerFactory {
@@ -16,19 +13,6 @@ public class SellerFactory {
         seller.setFollowerCount(0);
         seller.setFollowers(new ArrayList<>());
         return seller;
-    }
-
-    public static Seller createSellerWithFollowers(List<Buyer> buyers) {
-        Seller seller = createSeller();
-        for (Buyer buyer : buyers) {
-            seller.addFollower(buyer);
-        }
-        seller.setFollowerCount(buyers.size());
-        return seller;
-    }
-
-    public static Seller createSellerWithFollowers(Buyer... buyers) {
-        return createSellerWithFollowers(Arrays.asList(buyers));
     }
 }
 
