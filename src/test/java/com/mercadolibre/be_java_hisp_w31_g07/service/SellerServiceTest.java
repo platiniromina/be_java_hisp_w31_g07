c18a6aca7ab9b4385d64c0c5cf2ff6a29d702cfa
jcaparr <juancaparr0s@hotmail.com>
2025-04-30 12:34:45 -0300

    fix(SellerServiceTest): add unit test for getFollowers.

---
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g07/service/SellerServiceTest.java
@@ -0,0 +1,93 @@
+package com.mercadolibre.be_java_hisp_w31_g07.service;
+
+import com.mercadolibre.be_java_hisp_w31_g07.dto.request.SellerDto;
+import com.mercadolibre.be_java_hisp_w31_g07.dto.request.UserDto;
+import com.mercadolibre.be_java_hisp_w31_g07.exception.BadRequest;
+import com.mercadolibre.be_java_hisp_w31_g07.model.Buyer;
+import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
+import com.mercadolibre.be_java_hisp_w31_g07.repository.ISellerRepository;
+import com.mercadolibre.be_java_hisp_w31_g07.service.implementations.SellerService;
+import com.mercadolibre.be_java_hisp_w31_g07.util.BuyerFactory;
+import com.mercadolibre.be_java_hisp_w31_g07.util.SellerFactory;
+import com.mercadolibre.be_java_hisp_w31_g07.util.UserFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SellerServiceTest {
+    @InjectMocks
+    private SellerService sellerService;
+
+    @Mock
+    private IBuyerService buyerService;
+
+    @Mock
+    private IUserService userService;
+
+    @Mock
+    private ISellerRepository sellerRepository;
+
+    private Seller seller;
+    private Buyer buyer;
+    private UserDto user;
+    private UUID buyerId;
+    private UUID sellerId;
+
+    @BeforeEach
+    void setUp() {
+        seller = SellerFactory.createSeller();
+        buyer = BuyerFactory.createBuyer();
+        user = UserFactory.createUserDto();
+        buyerId = buyer.getId();
+        sellerId = seller.getId();
+    }
+
+    @Test
+    void testFindFollowersSucces() {
+        //Arrange
+        when(sellerRepository.findSellerById(sellerId)).thenReturn(Optional.of(seller));
+        when(userService.findById(sellerId)).thenReturn(user);
+
+        //Act
+        SellerDto result = sellerService.findFollowers(sellerId);
+
+        //Assert
+        assertNotNull(result);
+        assertAll(
+                () -> assertEquals(sellerId, result.getId()),
+                () -> assertEquals(user.getUserName(), result.getUserName()),
+                () -> assertEquals(seller.getFollowers().size(), result.getFollowerCount())
+        );
+        verify(sellerRepository).findSellerById(sellerId);
+        verify(userService, times(2)).findById(sellerId);
+        verifyNoMoreInteractions(sellerRepository, userService);
+    }
+
+    @Test
+    void testFindFollowersBadRequest() {
+        //Arrange
+        when(sellerRepository.findSellerById(sellerId)).thenReturn(Optional.empty());
+
+        //Act & Assert
+        BadRequest exception = assertThrows(BadRequest.class, () -> {
+            sellerService.findFollowers(sellerId);
+        });
+
+        assertEquals("Buyer: " + sellerId + " not found", exception.getMessage());
+        verifyNoInteractions(userService);
+        verify(sellerRepository).findSellerById(sellerId);
+        verifyNoMoreInteractions(sellerRepository);
+    }
+
+
+}
---
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g07/util/SellerFactory.java
@@ -0,0 +1,16 @@
+package com.mercadolibre.be_java_hisp_w31_g07.util;
+
+import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class SellerFactory {
+    public static Seller createSeller() {
+        Seller seller = new Seller();
+        seller.setId(UUID.randomUUID());
+        seller.setFollowerCount(0);
+        seller.setFollowers(new ArrayList<>());
+        return seller;
+    }
+}
