712347f84a90a63484609557e7ad26a40df4e5e9
thomasbarenghi <thomasbarenghi@gmail.com>
2025-04-30 13:04:57 -0300

    test: add unit tests for findPost method in PostService

---
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g07/service/PostServiceTest.java
@@ -0,0 +1,75 @@
+package com.mercadolibre.be_java_hisp_w31_g07.service;
+
+import com.mercadolibre.be_java_hisp_w31_g07.dto.response.PostResponseDto;
+import com.mercadolibre.be_java_hisp_w31_g07.exception.BadRequest;
+import com.mercadolibre.be_java_hisp_w31_g07.model.Post;
+import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
+import com.mercadolibre.be_java_hisp_w31_g07.repository.IPostRepository;
+import com.mercadolibre.be_java_hisp_w31_g07.service.implementations.PostService;
+import com.mercadolibre.be_java_hisp_w31_g07.util.GenericObjectMapper;
+import com.mercadolibre.be_java_hisp_w31_g07.util.PostFactory;
+import com.mercadolibre.be_java_hisp_w31_g07.util.SellerFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+    @Mock
+    private IPostRepository postRepository;
+
+    @Mock
+    private GenericObjectMapper mapper;
+
+    @InjectMocks
+    private PostService postService;
+
+    private Post post;
+    private UUID postId;
+    private Seller seller;
+
+    @BeforeEach
+    void setUp() {
+        seller = SellerFactory.createSeller();
+        post = PostFactory.createPost(seller.getId());
+        postId = post.getId();
+    }
+
+    @Test
+    void testFindPostSuccess() {
+        PostResponseDto postResponseDto = PostFactory.createPostResponseDto(seller.getId());
+        when(postRepository.findById(postId)).thenReturn(Optional.of(post));
+        when(mapper.map(post, PostResponseDto.class)).thenReturn(postResponseDto);
+
+        PostResponseDto result = postService.findPost(postId);
+
+        assertEquals(postResponseDto, result);
+        verify(postRepository).findById(postId);
+        verify(mapper).map(post, PostResponseDto.class);
+        verifyNoMoreInteractions(postRepository, mapper);
+    }
+
+    @Test
+    void testFindPostNotFound() {
+        when(postRepository.findById(postId)).thenReturn(Optional.empty());
+
+        Exception exception = assertThrows(BadRequest.class, () -> postService.findPost(postId));
+
+        assertEquals("Post " + postId + " not found", exception.getMessage());
+        verify(postRepository).findById(postId);
+        verifyNoMoreInteractions(postRepository);
+    }
+
+}
