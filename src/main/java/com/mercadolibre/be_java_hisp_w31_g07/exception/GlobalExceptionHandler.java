67ae8db6ced540123027ffe8b072db7ace263911
platini.romina <platini.romina@gmail.com>
2025-05-01 19:48:46 -0300

    chore(GlobalExceptionHandler): add ExceptionHandler to handle MethodArgumentNotValidException (jackarta validations)

--- src/main/java/com/mercadolibre/be_java_hisp_w31_g07/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w31_g07/exception/GlobalExceptionHandler.java
@@ -3,9 +3,12 @@ package com.mercadolibre.be_java_hisp_w31_g07.exception;
 import com.mercadolibre.be_java_hisp_w31_g07.dto.GlobalExceptioHandlerDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -21,5 +24,13 @@ public class GlobalExceptionHandler {
         String message = "Parameter '" + param + "' is invalid.";
         GlobalExceptioHandlerDto dto = new GlobalExceptioHandlerDto(message);
         return new ResponseEntity<>(dto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationErrors(MethodArgumentNotValidException ex) {
+        String errorMessage = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
