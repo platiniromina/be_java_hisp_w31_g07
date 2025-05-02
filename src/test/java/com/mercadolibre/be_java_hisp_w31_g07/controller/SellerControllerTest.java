e68a0e1e4ba57aae0656e41eb9f713c6678ec73a
CarolinaSeoane <carolina.b.seoane@gmail.com>
2025-05-02 01:01:58 -0300

    feat (SellerControllerTest): add integration test for Unfollow actions

--- src/test/java/com/mercadolibre/be_java_hisp_w31_g07/controller/SellerControllerTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w31_g07/controller/SellerControllerTest.java
@@ -4,6 +4,7 @@ import com.mercadolibre.be_java_hisp_w31_g07.model.Buyer;
 import com.mercadolibre.be_java_hisp_w31_g07.model.Seller;
 import com.mercadolibre.be_java_hisp_w31_g07.repository.ISellerRepository;
 import com.mercadolibre.be_java_hisp_w31_g07.repository.implementations.BuyerRepository;
+import com.mercadolibre.be_java_hisp_w31_g07.service.implementations.BuyerService;
 import com.mercadolibre.be_java_hisp_w31_g07.util.BuyerFactory;
 import com.mercadolibre.be_java_hisp_w31_g07.util.SellerFactory;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,6 +42,8 @@ class SellerControllerTest {
     private Buyer buyer;
     private Seller sellerWithBuyerFollower;
     private Buyer buyerWithFollowedSeller;
+    @Autowired
+    private BuyerService buyerService;
 
     @BeforeEach
     void setUp() {
@@ -54,7 +58,7 @@ class SellerControllerTest {
 
         sellerRepository.save(seller);
         buyerRepository.save(buyer);
-        
+
         sellerRepository.save(sellerWithBuyerFollower);
         buyerRepository.save(buyerWithFollowedSeller);
     }
@@ -98,9 +102,47 @@ class SellerControllerTest {
         assertBadRequestWithMessage(resultActions, "Buyer " + buyerId + " not found");
     }
 
+    @Test
+    @DisplayName("[SUCCESS] Unfollow seller")
+    void testUnfollowSellerSuccess() throws Exception {
+        ResultActions resultActions = performUnfollow(buyerWithFollowedSeller.getId(), sellerWithBuyerFollower.getId());
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("[ERROR] Unfollow seller - buyer is not following seller")
+    void testUnfollowSellerWhoImNotFollowingError() throws Exception {
+        ResultActions resultActions = performUnfollow(buyer.getId(), seller.getId());
+        assertBadRequestWithMessage(resultActions, "Buyer " + buyer.getId() + " is not following seller "
+                + seller.getId() + ". Unfollow action cannot be done");
+    }
+
+    @Test
+    @DisplayName("[ERROR] Unfollow seller - seller not found")
+    void testUnfollowSellerThatDoesNotExistError() throws Exception {
+        UUID sellerId = UUID.randomUUID();
+        ResultActions resultActions = performUnfollow(buyer.getId(), sellerId);
+        assertBadRequestWithMessage(resultActions, "Seller " + sellerId + " not found");
+    }
+
+    @Test
+    @DisplayName("[ERROR] Unfollow seller - buyer not found")
+    void testUnfollowSellerButBuyerNotFoundError() throws Exception {
+        UUID buyerId = UUID.randomUUID();
+        ResultActions resultActions = performUnfollow(buyerId, seller.getId());
+        assertBadRequestWithMessage(resultActions, "Buyer " + buyerId + " not found");
+    }
+
     private ResultActions performFollow(UUID buyerId, UUID sellerId) throws Exception {
         return mockMvc.perform(
                 post("/users/{buyerId}/follow/{sellerId}", buyerId, sellerId)
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+    }
+
+    private ResultActions performUnfollow(UUID buyerId, UUID sellerId) throws Exception {
+        return mockMvc.perform(
+                put("/users/{userId}/unfollow/{userIdToUnfollow}", buyerId, sellerId)
                         .contentType(MediaType.APPLICATION_JSON)
         ).andDo(print());
     }
@@ -111,4 +153,5 @@ class SellerControllerTest {
                         assertEquals(expectedMessage,
                                 Objects.requireNonNull(result.getResolvedException()).getMessage()));
     }
+
 }
